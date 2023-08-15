ab574b18a54cdad8ca58433eee9041f2fdabd1ab
Manuela <manupatino2006@gmail.com>
2023-08-15 12:08:18 -0500

    Initial Commit

---
+++ final proyect/final_proyectApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_proyectApp.swift
+//  final proyect
+//
+//  Created by MacBook Pro on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct final_proyectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
